b296e3421c220a90e03fa9436a703c994424bc2f
OhNishant <nishant.mishra550@gmail.com>
2024-03-16 20:10:16 +0530

    add: -or- in signin form

--- components/form/SignInForm.tsx
+++ components/form/SignInForm.tsx
@@ -85,6 +85,9 @@ const SignInForm = () => {
                         Forgot password?
                     </Link>
                 </div>
+                <div className="mx-auto my-4 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
+                    or
+                </div>
                 <div>
                     <GoogleButton />
                 </div>
