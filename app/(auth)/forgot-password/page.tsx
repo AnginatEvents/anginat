63d85696b67fc2efe4c20b1649d1238f0a43502e
OhNishant <nishant.mishra550@gmail.com>
2024-03-17 07:56:39 +0530

    feat: forgot password frontend

--- app/(auth)/forgot-password/page.tsx
+++ app/(auth)/forgot-password/page.tsx
@@ -1,7 +1,16 @@
 import ForgotPasswordForm from "@/components/form/ForgotPasswordForm";
 
 const Page = () => {
-    return <ForgotPasswordForm />;
+    return (
+        <>
+            <h1 className="font-bold text-3xl "> Forgot Password? </h1>
+            <p className="text-sm pb-5">
+                Don't worry! It happens. Please enter the email associated with
+                your account.{" "}
+            </p>
+            <ForgotPasswordForm />
+        </>
+    );
 };
 
 export default Page;
--- components/form/ForgotPasswordForm.tsx
+++ components/form/ForgotPasswordForm.tsx
@@ -1,5 +1,66 @@
-const ForgotPasswordForm = () => {
-    return <div>ForgotPasswordForm</div>;
+"use client";
+
+import React, { useState } from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { GoogleButton } from "@/components/ui/google-button";
+
+// Form components
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "../ui/button";
+import Link from "next/link.js";
+
+const formSchema = z.object({
+    email: z.string().email(),
+});
+
+const ForgotForm = () => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+        },
+    });
+    function onSubmit(data: z.infer<typeof formSchema>) {
+        console.log("Send otp to email");
+        console.log(data);
+    }
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormControl>
+                                <Input placeholder="Email" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+                <div>
+                    <Button
+                        type="submit"
+                        variant="default"
+                        className="self-stretch w-full"
+                    >
+                        Send OTP
+                    </Button>
+                </div>
+            </form>
+        </Form>
+    );
 };
 
-export default ForgotPasswordForm;
+export default ForgotForm;
