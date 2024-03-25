cc40b362fc97ee477a52fbbe726b8a62b57637db
OhNishant <nishant.mishra550@gmail.com>
2024-03-25 21:02:20 +0530

    fix: google oauth button actually functions now YAAAAY

--- components/ui/google-button.tsx
+++ components/ui/google-button.tsx
@@ -1,9 +1,15 @@
 import { Button, buttonVariants } from "@/components/ui/button";
 import Image from "next/image";
+import { signIn } from "next-auth/react";
 
 export function GoogleButton() {
+    const onClick = () => {
+        signIn("google", {
+            callbackUrl: "/",
+        });
+    };
     return (
-        <Button className="w-full " variant="outline">
+        <Button className="w-full" variant="outline" onClick={() => onClick()}>
             <Image src="google.svg" width={20} height={20} alt="Google Logo" />
             <span>Sign in with Google</span>
         </Button>
