c0b9bf4eca47ea00a9cab872f26795bdcdd8ee66
OhNishant <nishant.mishra550@gmail.com>
2024-03-25 20:59:35 +0530

    clear jank in auth options

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,17 +1,8 @@
 import type { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
-import { FirestoreAdapter } from "@auth/firebase-adapter";
-import { cert } from "firebase-admin/app";
 
 export const options: NextAuthOptions = {
-    // adapter: FirestoreAdapter({
-    //     credential: cert({
-    //         projectId: process.env.FIREBASE_PROJECT_ID!,
-    //         clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
-    //         privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
-    //     }),
-    // }),
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
