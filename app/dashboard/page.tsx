d9f3dabbfabde04182b79cb2402bf7f9a33368ef
OhNishant <nishant.mishra550@gmail.com>
2024-03-26 17:35:27 +0530

    add: some test content on dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,17 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+
 export default function Page() {
+    const session = useSession();
+
     return (
         <div>
             <h1>Dashboard</h1>
             <p>This is the dashboard page</p>
+            <p>
+                <strong>Session:</strong> {JSON.stringify(session)}
+            </p>
         </div>
     );
 }
