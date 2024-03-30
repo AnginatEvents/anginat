0bd5f59601fd7d59c8b2e69d416309ff63c830a0
OhNishant <nishant.mishra550@gmail.com>
2024-03-30 15:36:42 +0530

    refactor: providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import AuthProvider from "./context/AuthProvider";
+import AuthProvider from "@/providers/AuthProvider";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Script from "next/script";
