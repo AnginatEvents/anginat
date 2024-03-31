1a95fd7bb968b477681c659617c77b947126188d
OhNishant <nishant.mishra550@gmail.com>
2024-03-31 18:03:48 +0530

    fix: vertical alignment in dashboard logo

--- components/dashboard/LogoCard.tsx
+++ components/dashboard/LogoCard.tsx
@@ -3,7 +3,10 @@ import Link from "next/link";
 
 const LogoCard = () => {
     return (
-        <Link href="/dashboard" className="flex gap-3 py-4 pl-2 align-middle">
+        <Link
+            href="/dashboard"
+            className="flex items-center gap-3 py-4 pl-2 align-middle"
+        >
             <Infinity size="48px" strokeWidth="3px" />
             <strong className="text-4xl">Anginat</strong>
         </Link>
