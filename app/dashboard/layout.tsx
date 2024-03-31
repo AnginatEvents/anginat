9b4c9553443bef1e9b74f2e19d49e679b6e8b409
OhNishant <nishant.mishra550@gmail.com>
2024-03-31 12:42:16 +0530

    fix: unwanted gap in dashboard content

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,7 +2,7 @@ import Sidenav from "@/components/dashboard/Sidenav";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className="flex h-screen flex-row gap-3">
+        <div className="flex h-screen flex-row">
             <Sidenav />
             <main className="w-full bg-secondary">{children}</main>
         </div>
