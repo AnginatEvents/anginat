83a540640e2f63b6d70ba2bfbb81b8b7269bfc43
OhNishant <nishant.mishra550@gmail.com>
2024-04-01 10:31:53 +0530

    fix: #2 login form validatation on oauth button press

--- components/form/SignInForm.tsx
+++ components/form/SignInForm.tsx
@@ -85,10 +85,10 @@ const SignInForm = () => {
                 <div className="mx-auto my-4 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
                     or
                 </div>
-                <div>
-                    <GoogleButton />
-                </div>
             </form>
+            <div className="pt-4">
+                <GoogleButton />
+            </div>
         </Form>
     );
 };
