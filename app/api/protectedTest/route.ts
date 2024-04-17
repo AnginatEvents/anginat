75fce145b75fea5c43e3c1b1eb77208871b80593
OhNishant <nishant.mishra550@gmail.com>
2024-04-17 18:04:30 +0530

    add: api route to test the authentication on the server side

---
+++ app/api/protectedTest/route.ts
@@ -0,0 +1,9 @@
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+
+import { options } from "../auth/[...nextauth]/options";
+
+export async function GET() {
+    const session = await getServerSession(options);
+    return NextResponse.json({ session: session ?? "Not Logged In" });
+}
