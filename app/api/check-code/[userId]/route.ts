7ce95d6e1dbe3370b1c1449043b6434be028b32e
OhNishant <nishant.mishra550@gmail.com>
2024-04-23 20:54:04 +0530

    feat: check code api route userId is supposed to be a path param while code is passed in the body as formdata

---
+++ app/api/check-code/[userId]/route.ts
@@ -0,0 +1,61 @@
+import { NextRequest, NextResponse } from "next/server";
+import {
+    DynamoDBClient,
+    GetItemCommand,
+    GetItemCommandOutput,
+} from "@aws-sdk/client-dynamodb";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
+import { db } from "@/lib/db/dynamo_conn";
+
+export async function POST(
+    req: NextRequest,
+    { params }: { params: { userId: string } },
+) {
+    const formData = await req.formData();
+    const code = formData.get("code");
+
+    // const code = "00OQMXHALXF7";
+
+    console.log(params);
+    if (!code || typeof code !== "string") {
+        return NextResponse.json(
+            { error: "No code provided" },
+            { status: 400 },
+        );
+    }
+    const queryResponse = await getItemFromUserAndCode(params.userId, code);
+    if (!queryResponse) {
+        return NextResponse.json({ error: "No item found" }, { status: 400 });
+    }
+    console.log(queryResponse);
+    return NextResponse.json(
+        { message: "Item found", ...queryResponse },
+        { status: 200 },
+    );
+}
+
+async function getItemFromUserAndCode(userId: string, code: string) {
+    const params = {
+        TableName: "user-codes-single",
+        Key: {
+            pk: { S: `USER#${userId}` },
+            code: { S: code },
+        },
+    };
+    try {
+        const dbResponse: GetItemCommandOutput = await db.send(
+            new GetItemCommand(params),
+        );
+        if (dbResponse && dbResponse.Item) {
+            console.log("Item successfully retrieved from Dynamo db");
+            // console.log(dbResponse);
+            // console.log(dbResponse.Item);
+            return unmarshall(dbResponse.Item);
+        } else {
+            console.log("No item found in Dynamo db");
+            console.log(dbResponse);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
