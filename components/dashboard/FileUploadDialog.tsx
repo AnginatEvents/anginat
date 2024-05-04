5e23fdaf9ac5321869b361bab3c4ce91634244f9
OhNishant <nishant.mishra550@gmail.com>
2024-05-04 12:43:21 +0530

    download sample link in dialog

--- components/dashboard/FileUploadDialog.tsx
+++ components/dashboard/FileUploadDialog.tsx
@@ -26,16 +26,20 @@ const FileUploadDialog = ({
                     <DialogTitle>Import Codes via CSV</DialogTitle>
                     <DialogDescription>
                         You can use Google Sheets or Microsoft Excel to generate
-                        a csv file (.csv or .csv utf-8). Your file must have
-                        phone number with country code, name.It can also have
-                        custom columns that can be imported to Anginat as
-                        attriibutes. Download Sample CSV.
+                        a csv file (.csv or .csv utf-8). Make sure the
+                        Verification Code has the header "code" in all
+                        lowercase&nbsp;
+                        <a
+                            className="text-blue-500 underline"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="/sample.csv"
+                        >
+                            Download FIle
+                        </a>
+                        .
                     </DialogDescription>
                 </DialogHeader>
-                {
-                    //content
-                    // <Input className="h-[300px]" type="file" />
-                }
                 <FileDropzone uploadApiUrl={uploadApiUrl} />
             </DialogContent>
         </Dialog>
